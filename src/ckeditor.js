3d8ab34f37d75b3225644dbc9e71c3ea06c5437e
Sergey Glebko <you@example.com>
2018-11-03 17:31:42 +0300

    added icon

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -18,7 +18,7 @@ import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-// import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
@@ -51,6 +51,7 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	Table,
 	TableToolbar,
+	ImageUpload,
 	ImageUploader
 ];
 
